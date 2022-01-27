68a351d026a67500d191b0ddbeb2a223074fc496
Isaac Willian <isaaacwillian@gmail.com>
2022-01-26 23:39:33 -0300

    Desafio Aula 3 - Concluído

--- pages/chat.js
+++ pages/chat.js
@@ -16,83 +16,115 @@ export default function ChatPage() {
     setMensagem("");
   }
 
+  function removeMessage(id) {
+    let newListMessages = listMessages.filter((message) => {
+      if (message.id !== id) {
+        return message;
+      }
+    });
+    setListMessages(newListMessages);
+  }
+
   return (
-    <Box
-      styleSheet={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: appConfig.theme.colors.primary[500],
-        backgroundImage: `url(https://images.unsplash.com/photo-1511149755252-35875b273fd6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80)`,
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        backgroundBlendMode: "multiply",
-      }}
-    >
+    <>
+      <link
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        rel="stylesheet"
+      ></link>
       <Box
         styleSheet={{
           display: "flex",
-          flexDirection: "column",
-          flex: 1,
-          boxShadow: "0 2px 10px 0 rgb(0 0 0 / 20%)",
-          borderRadius: "5px",
-          backgroundColor: "hsla(230, 90%, 10%, 0.700)",
-          height: "100%",
-          maxWidth: "95%",
-          maxHeight: "95vh",
-          padding: "32px",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: appConfig.theme.colors.primary[500],
+          backgroundImage: `url(https://images.unsplash.com/photo-1511149755252-35875b273fd6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80)`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundBlendMode: "multiply",
         }}
       >
-        <Header />
         <Box
           styleSheet={{
-            position: "relative",
             display: "flex",
-            flex: 1,
-            height: "80%",
-            backgroundColor: "hsla(230, 90%, 70%, 0.100)",
             flexDirection: "column",
+            flex: 1,
+            boxShadow: "0 2px 10px 0 rgb(0 0 0 / 20%)",
             borderRadius: "5px",
-            padding: "16px",
+            backgroundColor: "hsla(230, 90%, 10%, 0.700)",
+            height: "100%",
+            maxWidth: "95%",
+            maxHeight: "95vh",
+            padding: "32px",
           }}
         >
-          <MessageList messages={listMessages} />
-
+          <Header />
           <Box
-            as="form"
             styleSheet={{
+              position: "relative",
               display: "flex",
-              alignItems: "center",
+              flex: 1,
+              height: "80%",
+              backgroundColor: "hsla(230, 90%, 70%, 0.100)",
+              flexDirection: "column",
+              borderRadius: "5px",
+              padding: "16px",
             }}
           >
-            <TextField
-              value={mensagem}
-              onChange={(event) => {
-                setMensagem(event.target.value);
-              }}
-              onKeyPress={(event) => {
-                if (event.key === "Enter") {
-                  event.preventDefault();
-                  handleNewMessage(mensagem);
-                }
-              }}
-              placeholder="Insira sua mensagem aqui..."
-              type="textarea"
-              styleSheet={{
-                width: "100%",
-                border: "0",
-                resize: "none",
-                borderRadius: "5px",
-                padding: "6px 8px",
-                backgroundColor: "hsla(230, 90%, 10%, 0.900)",
-                marginRight: "12px",
-                color: appConfig.theme.colors.neutrals["000"],
-              }}
+            <MessageList
+              messages={listMessages}
+              removeMessage={removeMessage}
             />
+
+            <Box
+              as="form"
+              styleSheet={{
+                display: "flex",
+              }}
+            >
+              <TextField
+                value={mensagem}
+                onChange={(event) => {
+                  setMensagem(event.target.value);
+                }}
+                onKeyPress={(event) => {
+                  if (event.key === "Enter") {
+                    event.preventDefault();
+                    handleNewMessage(mensagem);
+                  }
+                }}
+                placeholder="Insira sua mensagem aqui..."
+                type="textarea"
+                styleSheet={{
+                  width: "100%",
+                  border: "0",
+                  resize: "none",
+                  borderRadius: "5px",
+                  padding: "6px 8px",
+                  paddingRight: "45px",
+                  backgroundColor: "hsla(230, 90%, 10%, 0.900)",
+                  color: appConfig.theme.colors.neutrals["000"],
+                }}
+              />
+              <Button
+                onClick={() => {
+                  handleNewMessage(mensagem);
+                }}
+                colorVariant="neutral"
+                label={<span class="material-icons">send</span>}
+                styleSheet={{
+                  background: "none",
+                  position: "absolute",
+                  right: "15px",
+                  hover: {
+                    backgroundColor: appConfig.theme.colors.primary[400],
+                  },
+                }}
+              />
+            </Box>
           </Box>
         </Box>
       </Box>
-    </Box>
+    </>
   );
 }
 
@@ -123,6 +155,9 @@ function Header() {
           href="/"
           styleSheet={{
             color: appConfig.theme.colors.neutrals["200"],
+            hover: {
+              backgroundColor: appConfig.theme.colors.primary[700],
+            },
           }}
         />
       </Box>
@@ -155,13 +190,18 @@ function MessageList(props) {
               marginBottom: "12px",
               backgroundColor: "hsla(230, 90%, 10%, 0.500)",
               hover: {
-                backgroundColor: appConfig.theme.colors.neutrals[700],
+                backgroundColor: appConfig.theme.colors.primary[800],
               },
+              active: {
+                backgroundColor: appConfig.theme.colors.primary[800],
+              },
+              wordBreak: "break-all",
             }}
           >
             <Box
               styleSheet={{
                 marginBottom: "8px",
+                position: "relative",
               }}
             >
               <Image
@@ -185,6 +225,22 @@ function MessageList(props) {
               >
                 {new Date().toLocaleDateString()}
               </Text>
+              <Button
+                onClick={() => {
+                  props.removeMessage(message.id);
+                }}
+                colorVariant="neutral"
+                label={<span class="material-icons">clear</span>}
+                styleSheet={{
+                  background: "none",
+                  position: "absolute",
+                  right: "-7px",
+                  hover: {
+                    backgroundColor: appConfig.theme.colors.primary[400],
+                  },
+                  top: "-10px",
+                }}
+              />
             </Box>
             {message.text}
           </Text>
